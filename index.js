ccd0d20189e04c488fb7d5e972da3036ddb5dbcf
suhas sa <suhassa05@gmail.com>
2020-04-21 14:33:37 +0530

    auth

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const app = express();
-const setupDB = require("./config/database");
-const router = require("./config/routes");
-const port = 3043;
+const express=require('express')
+const cors=require('cors')
+const setupDB=require('./config/database')
+const router=require('./config/routes')
+const app=express()
+const port=3040
+app.use(express.json())
+app.use(cors())
+app.use('/',router)
+setupDB()
 
-app.use(express.json());
-app.use("/users", router);
-
-setupDB();
-app.listen(port, () => {
-  console.log('connected');
-});
+app.listen(port,()=>{
+    console.log('listening on port',port)
+})
